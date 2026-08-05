f7a36ce83206800b9b72fbf57e15078a8150e179
Alastor Kaneki <170276077+Alastor-Kaneki@users.noreply.github.com>
2026-08-05 00:21:59 -0400

    Describe Chrome MonkeyScript patch

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -3,7 +3,7 @@ group = "dev.alastorkaneki"
 patches {
     about {
         name = "Alastor Kaneki Morphe Patches"
-        description = "Morphe patches for downloading raw Opera GX mod packages."
+        description = "Morphe patches for Opera GX raw mod downloads and a monkey-style userscript/userstyle manager for Chrome Android."
         source = "https://github.com/Alastor-Kaneki/Morphe-Patches"
         author = "Alastor Kaneki"
         contact = "https://github.com/Alastor-Kaneki"
